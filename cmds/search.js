138a92a233c013d07aa5a6591456a77dfe86884a
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-06-20 22:31:36 -0430

    :star: Add search filtering and output. Also add colored output

--- cmds/search.js
+++ cmds/search.js
@@ -1,23 +1,33 @@
-'use strict';
+var fs = require('fs');
+var awm = require('../lib/awm');
+var _ = require('lodash');
 
 module.exports = function(program) {
 
 	program
-		.command('search <workflow>')
+		.command('search <keyword>')
 		.description('Search workflows on packal.org')
-		.action(function(workflow){
-			var fs = require('fs');
-			var awm = require('../lib/awm');
+		.action(function(keyword){
 
 			fs.readFile(awm.config.manifest, 'utf8', function (err, data) {
 			  if (err) {
-			    console.log('Couldn\'t read manifest file: ' + err);
+			    console.error(('Couldn\'t read manifest file: ' + err).bold.underline.red);
 			    process.exit(1);
 			  }
 
-			  var workflows = JSON.parse(data).manifest.workflow;
+			  var workflowList = JSON.parse(data).manifest.workflow;
 
-				//TODO: lodash filter here to obtain results
+				var filtered = _.filter(workflowList, function(workflow){
+					return (workflow.name[0].indexOf(keyword) > -1 || workflow.tags[0].indexOf(keyword) > -1);
+				});
+
+				if(filtered.length === 0) console.warn(('No worklflow found with the keyword ' + keyword.inverse).yellow);
+				else{
+					console.info('Workflows found: \n'.bold.cyan);
+					filtered.forEach(function(wf){
+						console.info(wf.bundle[0].cyan + ' -> ' + wf.name[0]);
+					});
+				}
 			});
 		});
 
