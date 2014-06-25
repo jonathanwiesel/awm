de7962f900e08a0287fc4a15da95eb4d7ed9e24a
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-06-25 13:38:37 -0430

    :star: Add list installed workflows command

---
+++ cmds/list.js
@@ -0,0 +1,31 @@
+var awm = require('../lib/awm');
+var fs = require('fs');
+var _ = require('lodash');
+var plist = require('plist');
+
+module.exports = function(program) {
+
+  program
+    .command('list')
+    .description('         List installed workflows.')
+    .action(function(){
+
+      fs.readdir(awm.config.workflowDir, function(err, dirList){
+        if(err){
+          console.error(('Error listing workflows directory: ' + err).red);
+          process.exit(1);
+        }
+
+        _(dirList).each(function(dir){
+
+          var plistFile = awm.config.workflowDir + dir + '/info.plist';
+          var settings = plist.parse(fs.readFileSync(plistFile, 'utf8'));
+
+          fs.exists(awm.config.workflowDir + dir + '/packal/', function(exists){
+            if(exists) console.info((settings.bundleid || 'NO-BUNDLE-ID') + ' => ' + settings.name);
+            else console.info('*'.yellow + (settings.bundleid || 'NO-BUNDLE-ID') + ' => ' + settings.name + ' (Not managed by packal)'.yellow);
+          });
+        });
+      });
+    });
+};
