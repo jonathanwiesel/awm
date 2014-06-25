6f0089cb4a822f7e773358b1a66a4308d7727472
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-06-25 16:09:51 -0430

    :star: Add caching directory creation on install

--- scripts/retrieve_list.js
+++ scripts/retrieve_list.js
@@ -5,6 +5,9 @@ var fs = require('fs');
 
 if (!fs.existsSync(awm.config.directory)) {
   fs.mkdirSync(awm.config.directory);
+  if(!fs.existsSync(awm.config.cacheDir)){
+    fs.mkdirSync(awm.config.cacheDir);
+  }
 }
 
 awm.fetchAndParseManifest(function(jsManifest){
