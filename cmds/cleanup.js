32069b41e3af69c7521bf2b63a754ce45c96dd7b
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-06-25 16:41:42 -0430

    :star: Add cleanup to delete cached downloads

---
+++ cmds/cleanup.js
@@ -0,0 +1,19 @@
+var awm = require('../lib/awm');
+var fs = require('fs');
+var _ = require('lodash')
+
+module.exports = function(program) {
+
+  program
+    .command('cleanup')
+    .description('Remove all cached downloads')
+    .action(function(){
+      fs.readdir(awm.config.cacheDir, function(err, cachedDownloads){
+        _(cachedDownloads).each(function(file){
+          console.info(('Removing ' + awm.config.cacheDir + file + '...').cyan);
+          fs.unlink(awm.config.cacheDir + file);
+        });
+      });
+    });
+
+};
