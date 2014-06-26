1d768f64bfbe6a701099782a83d2edabd433899d
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-06-25 20:38:06 -0430

    :star: Add packal home redirection

--- cmds/home.js
+++ cmds/home.js
@@ -1,23 +1,27 @@
 var awm = require('../lib/awm');
 var _ = require('lodash');
-var exec = require('child_process').exec
+var exec = require('child_process').exec;
 
 module.exports = function(program) {
 
   program
-    .command('home <bundleID>')
-    .description('Opens the workflow\'s Packal URL.')
+    .command('home [bundleID]')
+    .description('Opens the workflow\'s Packal URL. Or Packal home if none specified.')
     .action(function(bundleID){
 
-      awm.readManifest(function (workflowList) {
-        var selectedWF = _.find(workflowList, function(wf){
-          return (wf.bundle == bundleID);
-        });
+      if(!bundleID)
+        exec('open http://packal.org');
+      else{
+        awm.readManifest(function (workflowList) {
+          var selectedWF = _.find(workflowList, function(wf){
+            return (wf.bundle == bundleID);
+          });
 
-        if(!selectedWF)
-          console.warn(('There\'s no workflow with bundle ID ' + bundleID.inverse).yellow);
-        else
-          exec('open ' + selectedWF.url.replace(/"/g, '\\\"'));
-      });
+          if(!selectedWF)
+            console.warn(('There\'s no workflow with bundle ID ' + bundleID.inverse).yellow);
+          else
+            exec('open ' + selectedWF.url.replace(/"/g, '\\\"'));
+        });
+      }
     });
 };
