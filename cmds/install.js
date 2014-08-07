062f76b86d3c6345be915d6e6175d85cd491c73f
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-08-07 16:56:11 -0430

    :scissors: Modularize download for reuse in upgrade command

--- cmds/install.js
+++ cmds/install.js
@@ -1,9 +1,6 @@
 var awm = require('../lib/awm');
-var fs = require('fs');
 var _ = require('lodash');
-var request = require('request');
 var path = require('path');
-var ProgressBar = require('progress');
 var exec = require('child_process').exec
 
 module.exports = function(program) {
@@ -24,42 +21,9 @@ module.exports = function(program) {
           var downloadUrl = awm.config.packalUrl + selectedWF.bundle + '/' + selectedWF.file;
           var filePath = awm.config.cacheDir + path.basename(selectedWF.file, '.alfredworkflow') + '@' + selectedWF.version + '.alfredworkflow'
 
-          fs.exists(filePath, function(exists){
-            if(exists){
-
-              console.info(('Workflow cached at ' + filePath).cyan);
-              exec('open ' + filePath.replace(/"/g, '\\\"'));
-
-            }else{
-
-              var req = request(downloadUrl);
-              var bar;
-
-              req
-                .on('data', function(chunk){
-                  bar = bar || new ProgressBar('Downloading... [:bar] :percent :etas', {
-                    complete: '=',
-                    incomplete: ' ',
-                    width: 30,
-                    total: parseInt(req.response.headers['content-length'])
-                  });
-
-                  bar.tick(chunk.length);
-                })
-                .pipe(fs.createWriteStream(filePath))
-                .on('close', function (err) {
-                  if(err) console.error(('Error downloading file: ' + e).red)
-                  else {
-                    console.info(('Saved to ' + filePath).cyan);
-                    exec('open ' + filePath.replace(/"/g, '\\\"'));
-                  }
-
-                  bar.tick(bar.total - bar.curr);
-                });
-            }
+          awm.downloadFile(downloadUrl, filePath, function(err){
+            if(!err) exec('open ' + filePath.replace(/"/g, '\\\"'));
           });
-
-
         }
       });
     });
