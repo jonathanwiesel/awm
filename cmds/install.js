7c05c293cb3b066ca1fdbd97e38b4b3c78f116f5
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-08-08 08:42:47 -0430

    :star: Add already installed validation on install

--- cmds/install.js
+++ cmds/install.js
@@ -1,7 +1,7 @@
 var awm = require('../lib/awm');
 var _ = require('lodash');
 var path = require('path');
-var exec = require('child_process').exec
+var exec = require('child_process').exec;
 
 module.exports = function(program) {
 
@@ -18,11 +18,16 @@ module.exports = function(program) {
         if(!selectedWF) console.warn(('There\'s no workflow with bundle ID ' + workflow.inverse).yellow);
         else{
 
-          var downloadUrl = awm.config.packalUrl + selectedWF.bundle + '/' + selectedWF.file;
-          var filePath = awm.config.cacheDir + path.basename(selectedWF.file, '.alfredworkflow') + '@' + selectedWF.version + '.alfredworkflow'
+          awm.getWorkflowDir(selectedWF.bundle, function(dir){
+            if(dir) console.info((bundleID.inverse + ' is already installed').yellow);
+            else{
+              var downloadUrl = awm.config.packalUrl + selectedWF.bundle + '/' + selectedWF.file;
+              var filePath = awm.config.cacheDir + path.basename(selectedWF.file, '.alfredworkflow') + '@' + selectedWF.version + '.alfredworkflow';
 
-          awm.downloadFile(downloadUrl, filePath, function(err){
-            if(!err) exec('open ' + filePath.replace(/"/g, '\\\"'));
+              awm.downloadFile(downloadUrl, filePath, function(err){
+                if(!err) exec('open ' + filePath.replace(/"/g, '\\\"'));
+              });
+            }
           });
         }
       });
